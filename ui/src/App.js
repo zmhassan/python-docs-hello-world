3eb94c5ddd91bcf0cc74b777e2d9cdc34467b2aa
Zak Hassan <zak.hassan1010@gmail.com>
2021-11-11 20:03:31 -0800

    Deleting autogeneration

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import './App.css'
 import navigation from './components/navigation';
-from 
+ 
 function App() {
   return (
-    
-
     <div class="fullPage">
 
-      <navigation />
+         <navigation />
 
-<div class="container">
-     
-<div  class="text-center">
+          <div class="container">
+              
+              <div  class="text-center">
 
 
-<main role="main" class="inner cover">
-        <h1 class="cover-heading">Cover your page.</h1>
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-        <p class="lead">
-          <a href="/#learn" class="btn btn-lg btn-secondary">Learn more</a>
-        </p>
-      </main>
+                    <main role="main" class="inner cover">
+                      <h1 class="cover-heading">Cover your page.</h1>
+                      <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                      <p class="lead">
+                        <a href="/#learn" class="btn btn-lg btn-secondary">Learn more</a>
+                      </p>
+                    </main>
 
-</div>
-      </div>    
-     
+              </div>
+          </div>    
+              
     
     </div>
   );
