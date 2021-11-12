fa17f9d01a77d82c384d76ee49174720ba15d8e2
Zak Hassan <zak.hassan1010@gmail.com>
2021-11-11 19:52:14 -0800

    Adding better navigation system

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import './App.css'
-
+import navigation from './components/navigation';
+from 
 function App() {
   return (
     
 
     <div class="fullPage">
 
-      <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-            <header class="masthead mb-auto">
-              <div class="inner">
-                <h3 class="masthead-brand">Cover</h3>
-                <nav class="nav nav-masthead justify-content-center">
-                  <a class="nav-link active" href="/#home">  <i class="bi bi-house"></i> Home</a>
-                  <a class="nav-link" href="/#jobs"><i class="bi bi-box"></i> Jobs</a>
-                  <a class="nav-link" href="/#contact"> <i class="bi bi-send-x"></i>
-      Contact</a>
-                </nav>
-              </div>
-            </header>
-      </div>
-
+      <navigation />
 
 <div class="container">
      
---
+++ ui/src/components/navigation.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+
+class navigation extends Component {
+    render() {
+        return (
+            <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            <header class="masthead mb-auto">
+              <div class="inner">
+                <h3 class="masthead-brand">MetaCloud<h3>
+                <nav class="nav nav-masthead justify-content-center">
+                  <a class="nav-link active" href="/#home">  <i class="bi bi-house"></i> Home</a>
+                  <a class="nav-link" href="/#jobs"><i class="bi bi-box"></i> Jobs</a>
+                  <a class="nav-link" href="/#contact"> <i class="bi bi-send-x"></i>
+      Contact</a>
+                </nav>
+              </div>
+            </header>
+            </div>
+
+        );
+    }
+}
+
+export default navigation;
